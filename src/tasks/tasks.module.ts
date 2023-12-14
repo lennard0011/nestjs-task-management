d8071aac9b773332df9e7f34132347e5e2e38123
Lennard van der Plas <lennardplas@gmail.com>
2023-12-14 19:44:18 +0100

    add env variables via .env

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -4,6 +4,7 @@ import { TasksService } from './tasks.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksRepository } from './tasks.repository';
 import { AuthModule } from 'src/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [TypeOrmModule.forFeature([TasksRepository]), AuthModule],
